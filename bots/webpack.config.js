6bbce994a0d59364daa47626a6d0eb5c07d094bf
Rasmus Kobber <rasmusrk@gmail.com>
2017-12-28 13:17:29 +0100

    added webpack config

---
+++ bots/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const entries = {
+  artemis: './bots/artemis.ts'
+}
+
+module.exports = {
+  entry: entries,
+  output: {
+    path: path.resolve(__dirname),
+    filename: "[name].bot.js", 
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: [".ts"]
+  },
+
+  target: 'webworker'
+}
